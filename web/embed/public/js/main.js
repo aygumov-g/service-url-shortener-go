e1acf390945f23646eb1687fdaa2ac27965399fd
Gadzhi <gaadjik05@gmail.com>
2026-03-15 10:03:59 +0300

    update frontend architecture

--- web/embed/public/js/main.js
+++ web/embed/public/js/main.js
@@ -3,25 +3,24 @@ new class {
         this.data = data;
         this.data.input.addEventListener("input", () => {
             if (this.data["input"].value.length > 0) {
-                this.hideDescriptionText();
-                this.showFooterText();
+                this.hideDescriptionTextContainer();
+                this.showFooterTextContainer();
                 this.hideErrorText();
             } else {
-                this.showDescriptionText();
-                this.hideFooterText();
+                this.showDescriptionTextContainer();
+                this.hideFooterTextContainer();
             };
-            this.hideResultBlock();
-            this.hideResultDescriptionText();
-            this.hideResultTitleText();
+            this.hideSection_2Container();
         });
         this.data.postButton.addEventListener("click", () => {
             if (!this.data.input.value.trim().length === 0 || !this.data.input.value.includes(".")) {
                 this.showErrorText("Некорректная ссылка");
+                this.hideFooterTextContainer();
             } else {
                 this.post(this.data.input.value);
-            }
+            };
         });
-        this.data.copyButton.addEventListener("click", () => {
+        this.data.copyButton_1.addEventListener("click", () => {
             try {
                 navigator.clipboard.writeText(this.data.linkURL.href.replace("xn--q1a", "с")).then(() => {
                     alert("Скопировано!");
@@ -30,75 +29,56 @@ new class {
                 });
             } catch (error) {
                 alert(`Ошибка копирования: ${error}`)
-            }
+            };
         });
         this.hideErrorText();
-        this.hideFooterText();
-        this.hideResultTitleText();
-        this.hideResultDescriptionText();
-        this.hideResultBlock();
+        this.hideFooterTextContainer();
+        this.hideSection_2Container();
         if (document.location.search.length > 0 && document.location.search[0] === "?") {
             var args = document.location.search.split("=")
             if (args.length === 2 && args[0].length === 2 && args[0][1] == "c" && args[1].length > 0) {
                 this.get(args[1]);
-            }
-        }
+            };
+        };
     };
     showErrorText(text) {
-        this.data.errorText.classList.add("shortener__error-text-show");
-        this.data.errorText.classList.remove("shortener__error-text-hide");
+        this.data.errorTextContainer.classList.add("default-show");
+        this.data.errorTextContainer.classList.remove("default-hide");
         this.data.errorText.textContent = text;
-        this.hideFooterText();
     };
     hideErrorText() {
-        this.data.errorText.classList.add("shortener__error-text-hide");
-        this.data.errorText.classList.remove("shortener__error-text-show");
+        this.data.errorTextContainer.classList.add("default-hide");
+        this.data.errorTextContainer.classList.remove("default-show");
         this.data.errorText.textContent = "";
     };
-    showFooterText() {
-        this.data.footerText.classList.add("shortener__footer-text-show");
-        this.data.footerText.classList.remove("shortener__footer-text-hide");
+    showDescriptionTextContainer() {
+        this.data.descriptionTextContainer.classList.add("default-show");
+        this.data.descriptionTextContainer.classList.remove("default-hide");
     };
-    hideFooterText() {
-        this.data.footerText.classList.add("shortener__footer-text-hide");
-        this.data.footerText.classList.remove("shortener__footer-text-show");
+    hideDescriptionTextContainer() {
+        this.data.descriptionTextContainer.classList.add("default-hide");
+        this.data.descriptionTextContainer.classList.remove("default-show");
     };
-    showDescriptionText() {
-        this.data.descriptionText.classList.add("shortener__description-text-show");
-        this.data.descriptionText.classList.remove("shortener__description-text-hide");
+    showFooterTextContainer() {
+        this.data.footerTextContainer.classList.add("default-show");
+        this.data.footerTextContainer.classList.remove("default-hide");
     };
-    hideDescriptionText() {
-        this.data.descriptionText.classList.add("shortener__description-text-hide");
-        this.data.descriptionText.classList.remove("shortener__description-text-show");
+    hideFooterTextContainer() {
+        this.data.footerTextContainer.classList.add("default-hide");
+        this.data.footerTextContainer.classList.remove("default-show");
     };
-    showResultTitleText() {
-        this.data.resultTitleText.classList.add("shortener__result-title-text-show");
-        this.data.resultTitleText.classList.remove("shortener__result-title-text-hide");
+    showSection_2Container() {
+        this.data.section_2Container.classList.add("default-show");
+        this.data.section_2Container.classList.remove("default-hide");
     };
-    hideResultTitleText() {
-        this.data.resultTitleText.classList.add("shortener__result-title-text-hide");
-        this.data.resultTitleText.classList.remove("shortener__result-title-text-show");
-    };
-    showResultDescriptionText() {
-        this.data.resultDescriptionText.classList.add("shortener__result-description-text-show");
-        this.data.resultDescriptionText.classList.remove("shortener__result-description-text-hide");
-    };
-    hideResultDescriptionText() {
-        this.data.resultDescriptionText.classList.add("shortener__result-description-text-hide");
-        this.data.resultDescriptionText.classList.remove("shortener__result-description-text-show");
-    };
-    showResultBlock() {
-        this.data.resultBlock.classList.add("shortener__result-block-show");
-        this.data.resultBlock.classList.remove("shortener__result-block-hide");
-    };
-    hideResultBlock() {
-        this.data.resultBlock.classList.add("shortener__result-block-hide");
-        this.data.resultBlock.classList.remove("shortener__result-block-show");
+    hideSection_2Container() {
+        this.data.section_2Container.classList.add("default-hide");
+        this.data.section_2Container.classList.remove("default-show");
     };
     setLink(url) {
         this.data.linkText.textContent = url.replace("https://", "");
         this.data.linkURL.href = url;
-    }
+    };
     async post(url) {
         try {
             const response = await fetch("/api/links", {
@@ -114,9 +94,11 @@ new class {
                 document.location.href = `/?c=${(await response.json())["short_code"]}`;
             } else {
                 this.showErrorText(`Ошибка с сервера: ${await response.text()}`);
+                this.hideFooterTextContainer();
             }
         } catch {
             this.showErrorText("Сервер не ответил");
+            this.hideFooterTextContainer();
         };
     };
     async get(short_code) {
@@ -125,10 +107,8 @@ new class {
                 method: "GET",
             });
             if (response.ok) {
-                this.hideDescriptionText();
-                this.showResultTitleText();
-                this.showResultDescriptionText();
-                this.showResultBlock();
+                this.hideDescriptionTextContainer();
+                this.showSection_2Container();
                 ((reponse_data) => {
                     this.data.input.value = reponse_data["original_url"];
                     this.setLink(reponse_data["short_url"]);
@@ -136,20 +116,19 @@ new class {
             } else {
                 document.location.href = "/";
             }
-        } catch {
+        } catch (error) {
             document.location.href = "/";
         };
-    }
+    };
 }({
     input: document.getElementById("inp"),
+    postButton: document.getElementById("postBtn"),
+    copyButton_1: document.getElementById("copyBtn_1"),
+    errorText: document.getElementById("errorText"),
+    errorTextContainer: document.getElementById("errorTextContainer"),
+    descriptionTextContainer: document.getElementById("descriptionTextContainer"),
+    footerTextContainer: document.getElementById("footerTextContainer"),
+    section_2Container: document.getElementById("section_2Container"),
     linkURL: document.getElementById("linkURL"),
     linkText: document.getElementById("linkText"),
-    postButton: document.getElementById("postBtn"),
-    copyButton: document.getElementById("copyBtn"),
-    errorText: document.getElementsByClassName("shortener__error-text")[0],
-    footerText: document.getElementsByClassName("shortener__footer-text")[0],
-    descriptionText: document.getElementsByClassName("shortener__description-text")[0],
-    resultTitleText: document.getElementsByClassName("shortener__result-title-text")[0],
-    resultDescriptionText: document.getElementsByClassName("shortener__result-description-text")[0],
-    resultBlock: document.getElementsByClassName("shortener__result-block")[0],
 });
